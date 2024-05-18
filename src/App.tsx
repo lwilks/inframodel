8567128322022d7aa205f212518175024941a421
Liam Wilks <liam@liamwilks.onmicrosoft.com>
2024-05-18 23:21:56 +1000

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import * as OBC from 'openbim-components';
+
+const App: React.FC = () => {
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const container = containerRef.current;
+
+        if (container) {
+            const components = new OBC.Components();
+            components.scene = new OBC.SimpleScene(components);
+            components.renderer = new OBC.SimpleRenderer(components, container);
+            components.camera = new OBC.SimpleCamera(components);
+            components.raycaster = new OBC.SimpleRaycaster(components);
+
+            components.init();
+
+            const scene = components.scene.get();
+            components.camera.controls.setLookAt(10, 10, 10, 0, 0, 0);
+
+            const grid = new OBC.SimpleGrid(components);
+            scene.add(grid.get());
+
+            const boxMaterial = new THREE.MeshStandardMaterial({ color: '#6528D7' });
+            const boxGeometry = new THREE.BoxGeometry(3, 3, 3);
+            const cube = new THREE.Mesh(boxGeometry, boxMaterial);
+            cube.position.set(0, 1.5, 0);
+            scene.add(cube);
+
+            components.scene.setup();
+
+            return () => {
+                components.dispose();
+            };
+        }
+    }, []);
+
+    return <div ref={containerRef} id="container" style={{ width: '100%', height: '100vh' }} />;
+};
+
+export default App;
