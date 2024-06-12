7a0a4ddf3a91506a28b1f3c8d4981aef52668285
lwilks <liam.wilks@gmail.com>
2024-06-12 12:16:42 +0000

    Load LandXML

--- src/ThreeDView.tsx
+++ src/ThreeDView.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+//import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import * as OBC from 'openbim-components';
-import { toGlb } from "landxml";
+// @ts-ignore
 import sampleLandXML from './assets/sample.xml';
+//import * as xml2js from 'xml2js';
+import { XMLParser } from 'fast-xml-parser';
 
 const ThreeDView: React.FC = () => {
     const containerRef = useRef<HTMLDivElement>(null);
@@ -19,55 +21,123 @@ const ThreeDView: React.FC = () => {
 
             components.init();
 
+            console.log('LWTEST!');
+
             const scene = components.scene.get();
             (components.camera as any).controls.setLookAt(10, 10, 10, 0, 0, 0);
 
-            const grid = new OBC.SimpleGrid(components);
-            scene.add(grid.get());
+            //const grid = new OBC.SimpleGrid(components);
+            //scene.add(grid.get());
 
             const loadModel = async () => {
-                try {
-                    // Read the contents of the sampleLandXML file into string
+                try {                   
                     const response = await fetch(sampleLandXML);
+                    console.log('HERE!');                    
+                    if (!response.ok) {
+                        console.error('Network response was not ok');
+                        return;
+                    }
+                
                     const xmlString = await response.text();
-                    
-                    // Ensure sampleLandXML is correctly fetched or required
-                    const center = "auto";
-                    const glbSurfaces = await toGlb(xmlString, center);
-                    let { glb } = glbSurfaces[0];
+                    const parser = new XMLParser();
+                
+                    let jsonObj;
+                    try {
+                        jsonObj = parser.parse(xmlString);
+                    } catch (error) {
+                        console.error('Failed to parse XML:', error);
+                    }
 
-                    // Convert the Uint8Array to a Blob
-                    const blob = new Blob([glb], { type: 'model/gltf-binary' });
-                    // Create an object URL for the Blob
-                    const url = URL.createObjectURL(blob);
+                    //Extract points from the first surface in jsonObj
+                    const points = jsonObj['LandXML']['Surfaces']['Surface']['Definition']['Pnts']['P'];
 
-                    const loader = new GLTFLoader();
-                    loader.load(url, (gltf) => {
-                        const model = gltf.scene;
-                        scene.add(model);
-                    }, undefined, (error) => {
-                        console.error('An error happened', error);
+                    //Find the centroid of the points
+                    let xSum = 0;
+                    let ySum = 0;
+                    let zSum = 0;
+                    let minY = Infinity;
+                    let maxY = -Infinity;
+                    points.forEach((point: any) => {
+                        let pts = point.split(' ');
+                        const y = parseFloat(pts[2]);
+                        xSum += parseFloat(pts[1]);
+                        ySum += y;
+                        zSum += parseFloat(pts[0]);
+                        minY = Math.min(minY, y);
+                        maxY = Math.max(maxY, y);
                     });
+                    const range = maxY - minY;
+                    const xCentroid = xSum / points.length;
+                    const yCentroid = ySum / points.length;
+                    const zCentroid = zSum / points.length;
+                    console.log('Centroid:', xCentroid, yCentroid, zCentroid);
+                    // Initialize array of points for BufferGeometry
+                    let tempVertices: number[] = [];
+                    points.forEach((point: any) => {
+                        let pts = point.split(' ');
+                        let x = parseFloat(pts[1]) - xCentroid;
+                        let y = parseFloat(pts[2]) - yCentroid;
+                        let z = parseFloat(pts[0]) - zCentroid;
+                        tempVertices.push(x, y, z);
+                    });
+
+                    const faces = jsonObj['LandXML']['Surfaces']['Surface']['Definition']['Faces']['F'];
+                    const facesArray: number[] = [];
+                    faces.forEach((face: any) => {
+                        const faceArray = face.split(' ');
+                        facesArray.push(faceArray[0]-1, faceArray[1]-1, faceArray[2]-1);
+                    });
+
+                    // Create a BufferGeometry and set the vertices and indices
+                    const geometry = new THREE.BufferGeometry();
+                    geometry.setAttribute('position', new THREE.Float32BufferAttribute(tempVertices, 3));
+                    geometry.setIndex(facesArray);
+
+                    // Create a material and set the color
+                    //const material = new THREE.MeshStandardMaterial({ color: '#FF0000', vertexColors: true});
+                    
+                    // Colorize the mesh according to height
+                    let colors: number[] = [];
+                    tempVertices.forEach((_, index) => {
+                        const height = tempVertices[index * 3 + 2];
+                        const normHeight = (height - (minY- yCentroid)) / range;
+                        const r = normHeight + 0.5;
+                        const g = normHeight + 0.5;
+                        const color = new THREE.Color();
+                        color.setRGB( r, g, 1, THREE.SRGBColorSpace );
+                        colors.push(color.r, color.g, color.b);
+                    });
+                    geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+
+                    const material = new THREE.MeshPhongMaterial( {
+                        side: THREE.DoubleSide,
+                        vertexColors: true,
+                        wireframe: true
+                    } );
+
+                    // Create a mesh and add it to the scene
+                    const mesh = new THREE.Mesh(geometry, material);
+                    scene.add(mesh);              
                 } catch (error) {
                     console.error('Failed to load model:', error);
                 }
             };
             loadModel();
 
-            const boxMaterial = new THREE.MeshStandardMaterial({ color: '#6528D7' });
-            const boxGeometry = new THREE.BoxGeometry(3, 3, 3);
-            const cube = new THREE.Mesh(boxGeometry, boxMaterial);
-            cube.position.set(0, 1.5, 0);
-            scene.add(cube);
+            //const boxMaterial = new THREE.MeshStandardMaterial({ color: '#6528D7' });
+            //const boxGeometry = new THREE.BoxGeometry(3, 3, 3);
+            //const cube = new THREE.Mesh(boxGeometry, boxMaterial);
+            //cube.position.set(0, 1.5, 0);
+            //scene.add(cube);
 
             (components.scene as any).setup();
 
-            const label = new OBC.Simple2DMarker(components);
-            label.get().position.set(0, 4, 0);
-            label.get().element.textContent = 'Hello, Components!';
-            label.get().element.addEventListener('pointerdown', () => {
-            console.log('Hello, Components!');
-            });            
+            //const label = new OBC.Simple2DMarker(components);
+            //label.get().position.set(0, 4, 0);
+            //label.get().element.textContent = 'Hello, Components!';
+            //label.get().element.addEventListener('pointerdown', () => {
+            //console.log('Hello, Components!');
+            //});            
 
             return () => {
             (components.camera as any).dispose();  
